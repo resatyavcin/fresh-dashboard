3b9412a4ef3169e6380a29ddb95caea9d9ca0331
TFa3sfQFw69zbYAW <resatyavcin@icloud.com>
2022-06-23 23:52:04 +0300

    eslint and prettier setup

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    tabWidth: 4, //Girinti belirlenir
+    semi: true, // Noktalı virgül belirlenir
+    singleQuote: true, // Tek tırnak yapar
+    printWidth: 120, //max-length belirlenir
+    bracketSpacing: true, // objelerde curly braces arasındakine bitişi ve başına bir karakter boşluk koyar.
+    endOfLine: 'auto',
+    arrowFunctionParantheses: 'always',
+    trailingComma: 'all',
+};
